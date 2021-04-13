66cad6bad7230aa201e01da0a98b2dd61ff79362
3xP3rT <3xP3rT@DESKTOP-FSHNI2D>
2021-04-14 01:07:10 +0600

    Inicial-create-commit

---
+++ CleanArchitecture/CleanArchitecture.Mvc/Data/Migrations/20210413190329_Inicial-Create.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchitecture.Mvc.Data.Migrations
+{
+    public partial class InicialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
